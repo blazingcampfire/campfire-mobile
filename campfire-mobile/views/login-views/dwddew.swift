37d35814b32926bb5572dff191f5feb750890f8e
David <david.adebogun@yale.edu>
2023-06-17 22:09:22 -0400

    test

---
+++ campfire-mobile/views/login-views/dwddew.swift
@@ -0,0 +1,20 @@
+//
+//  dwddew.swift
+//  campfire-mobile
+//
+//  Created by Femi Adebogun on 6/17/23.
+//
+
+import SwiftUI
+
+struct dwddew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct dwddew_Previews: PreviewProvider {
+    static var previews: some View {
+        dwddew()
+    }
+}

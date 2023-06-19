636eb3feb62075911bcc699cb19ac738d1fe7334
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-06-19 14:51:25 -0500

    Created  "upload profile pic page"  view after creating account and choosing username

---
+++ campfire-mobile/views/login-views/SetProfilePic.swift
@@ -0,0 +1,49 @@
+//
+//  SetProfilePic.swift
+//  campfire-mobile
+//
+//  Created by Adarsh G on 6/19/23.
+//
+
+import SwiftUI
+
+struct SetProfilePic: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color(.init(red: 255/255, green: 50/255, blue: 89/255, alpha: 1)), Color(.init(red: 255/255, green: 153/255, blue: 102/255, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.vertical)
+            .overlay(
+                VStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    
+                    VStack(spacing: 40) {
+                        Text("upload a profile pic")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Futura-Bold", size: 25))
+                            .padding(.top, 100)
+                        
+                        Circle()
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 200)
+                            
+                        Text("you're ready!")
+                            .foregroundColor(Color.white)
+                            .font(.custom("Futura-Bold", size: 13))
+                        
+                        LFButton(text: "next"){}
+
+                    }
+                    .padding(.bottom, 300)
+                    
+                    
+                }
+            )
+    }
+}
+
+struct SetProfilePic_Previews: PreviewProvider {
+    static var previews: some View {
+        SetProfilePic()
+    }
+}

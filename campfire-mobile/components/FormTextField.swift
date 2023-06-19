570cd2ea0de2984d42c543ac3df34619314b38e8
toni-akintola <oakintol@nd.edu>
2023-06-18 22:41:06 -0400

    reverted formtextfield changes because of a bug with the foreground color

--- campfire-mobile/components/FormTextField.swift
+++ campfire-mobile/components/FormTextField.swift
@@ -6,23 +6,13 @@ struct FormTextField: View {
     var placeholderText: String
     
     var body: some View {
-        HStack {
-            Form {
-                TextField(placeholderText, text: $text)
-                    .font(.custom("Futura-Bold", size: 20))
-                    .background(Theme.Peach)
-                    .foregroundColor(Theme.Peach)
-                    
-            }
-            .foregroundColor(Theme.Peach)
-            .textFieldStyle(.plain)
+        TextField(placeholderText, text: $text)
+            .font(.custom("Futura-Bold", size: 20))
+            .frame(width: 300, height: 10, alignment: .center)
+            .foregroundColor(Color.white)
+            .padding()
             .background(Theme.Peach)
-        }
-        .frame(width: 300, height: 10, alignment: .center)
-        .foregroundColor(Color.white)
-        .padding()
-        .background(Theme.Peach)
-        .cornerRadius(16)
+            .cornerRadius(16)
     }
 }
 

8f217d4866192d496530c95d8afb41fcf5735227
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-06-19 23:16:12 -0500

    made changes to profile page (added bio)

--- campfire-mobile/views/app-main-views/ProfilePage.swift
+++ campfire-mobile/views/app-main-views/ProfilePage.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProfilePage: View {
     let username: String = "@david_adegangbanger"
     let profilePicture: String = "ragrboard"
+    let bio: String = "Bio"
     let postImages: [String] = [
         "ragrboard",
         "ragrboard",
@@ -20,59 +21,54 @@ struct ProfilePage: View {
     ]
 
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [Color(.init(red: 255/255, green: 50/255, blue: 89/255, alpha: 1)), Color(.init(red: 255/255, green: 153/255, blue: 102/255, alpha: 1))]), startPoint: .top, endPoint: .bottom)
-            .edgesIgnoringSafeArea(.vertical)
-            .overlay(
-                VStack {
-                    Image(profilePicture)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                    
-                    Text(username)
-                        .font(.custom("Futura-Bold", size: 20))
-                        .foregroundColor(.white)
-                    
-                    Divider()
-                        .background(Color.white)
-                    
-                    Text("Posts")
-                        .font(.custom("Futura-Bold", size: 15))
-                        .foregroundColor(.white)
-                    
-                    ScrollView(.vertical, showsIndicators: false) {
-                        LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)], spacing: 10) {
-                            ForEach(0..<postImages.count, id: \.self) { index in
-                                if index % 2 == 0 {
-                                    VStack(spacing: 10) {
-                                        Image(postImages[index])
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+        VStack {
+            
+            Image(profilePicture)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
 
-                                        
-                                        if index + 1 < postImages.count {
-                                            Image(postImages[index + 1])
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                        }
-                                    }
-                                }
-                            }
+            Text(username)
+                .font(.custom("Futura-Bold", size: 15))
+                .foregroundColor(.black)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.white)
+                .shadow(color: Theme.Apricot,
+                        radius: 2)                .frame(height: 75)
+                .overlay(Text(bio))
+
+            Divider()
+                .background(Theme.Peach)
+
+            Text("Posts")
+                .font(.custom("Futura-Bold", size: 15))
+                .foregroundColor(.white)
+
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: [GridItem(.flexible(), spacing: 10)], spacing: 60) {
+                    ForEach(0..<postImages.count, id: \.self) { index in
+                        VStack(spacing: 10) {
+                            Image(postImages[index])
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(RoundedRectangle(cornerRadius: 25))
+                                .shadow(color: Theme.Apricot, radius: 5)
                         }
-                        .padding(.horizontal)
                     }
-                    .frame(height: 500)
-                    
-                    Spacer()
                 }
-                .padding()
-            )
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(height: 600)
+
+            Spacer()
+        }
+        .padding()
     }
-   
 }
+
 
 
 

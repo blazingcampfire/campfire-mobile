b341cdd9954a89eb83a8ceb912bf3d91177496c0
davidadebogun <david.adebogun@yale.edu>
2023-06-23 13:50:15 -0400

    settings page made

---
+++ campfire-mobile/views/app-main-views/SettingsPage.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsPage.swift
+//  campfire-mobile
+//
+//  Created by Femi Adebogun on 6/23/23.
+//
+
+import SwiftUI
+
+struct SettingsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsPage()
+    }
+}

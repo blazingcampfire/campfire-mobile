38be9cb50fb261f57d1c8cb3fe4e255a7dd1ee74
davidadebogun <david.adebogun@yale.edu>
2023-06-23 13:50:56 -0400

    tt

--- campfire-mobile/views/app-main-views/SettingsPage.swift
+++ campfire-mobile/views/app-main-views/SettingsPage.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct SettingsPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            NavigationView {
+                
+                Form {
+                    Section(header: Text("Display")) {
+                        
+                            Toggle(isOn: .constant(false)) {
+                                Label("Dark Mode", systemImage: "moon")
+                            }
+                            
+                            Toggle(isOn: .constant(true)) {
+                                Label("Notifications", systemImage: "bell.fill")
+                            }
+                        }
+                        
+                        .font(.custom("LexendDeca", size: 16))
+                        Section(header: Text("Information")) {
+                            Label("FAQ", systemImage: "doc.fill")
+                            Text("Privacy Policy")
+                            Text("Terms of Service")
+                        }
+                        .font(.custom("LexendDeca", size: 16))
+                    
+                
+                    Section(header: Text("Issues")) {
+                        Label("Report Account", systemImage: "exclamationmark.triangle.fill")
+                        Text("Contact Us")
+                    }
+                    .font(.custom("LexendDeca", size: 16))
+                    
+                    Section(header: Text("Account Permissions")) {
+                        Text("Log Out")
+                        Text("Delete Account")
+                    }
+                    .font(.custom("LexendDeca", size: 16))
+                    
+                }
+                .navigationTitle("Settings").font(.custom("LexendDeca", size: 25))
+            }
     }
 }
 

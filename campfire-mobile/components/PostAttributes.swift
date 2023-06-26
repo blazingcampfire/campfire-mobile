867077336c54bfaf8bb87b55861b1ded5f3ffaf7
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-06-26 14:04:40 -0500

    made changes to post section of profile page

---
+++ campfire-mobile/components/PostAttributes.swift
@@ -0,0 +1,20 @@
+//
+//  PostAttributes.swift
+//  campfire-mobile
+//
+//  Created by Adarsh G on 6/26/23.
+//
+
+import SwiftUI
+
+struct PostAttributes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostAttributes_Previews: PreviewProvider {
+    static var previews: some View {
+        PostAttributes()
+    }
+}

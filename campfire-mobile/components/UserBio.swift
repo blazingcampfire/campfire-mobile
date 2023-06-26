517f4abe7f59dab7b03febf99ef05b76fcab3870
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-06-25 23:53:09 -0500

    Made changes to the profile page

---
+++ campfire-mobile/components/UserBio.swift
@@ -0,0 +1,20 @@
+//
+//  userBio.swift
+//  campfire-mobile
+//
+//  Created by Adarsh G on 6/25/23.
+//
+
+import SwiftUI
+
+struct userBio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct userBio_Previews: PreviewProvider {
+    static var previews: some View {
+        userBio()
+    }
+}
---
+++ campfire-mobile/components/UserProfilePic.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfilePic.swift
+//  campfire-mobile
+//
+//  Created by Adarsh G on 6/25/23.
+//
+
+import SwiftUI
+
+struct UserProfilePic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfilePic_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfilePic()
+    }
+}

a11576e2aa81e8fa1dd774e7ca9f6ac535eda028
davidadebogun <david.adebogun@yale.edu>
2023-06-26 00:39:02 -0400

    merge changes

--- campfire-mobile/views/login-views/VerifyEmail.swift
+++ campfire-mobile/views/login-views/VerifyEmail.swift
@@ -22,26 +22,16 @@ struct VerifyEmail: View {
                                 .padding(10)
                                 
                             
-<<<<<<< HEAD
-                            Text("choose an option below to verify your email:") .font(.custom("LexendDeca-Bold", size: 20))
-                                .frame(width: 380, alignment: .leading)
-                                .foregroundColor(Color.white).padding(5).offset(x: 20, y: -80).multilineTextAlignment(.center)
-=======
                             Text("choose an option below to verify your email:") .font(.custom("LexendDeca-Bold", size: 25))
                                 .frame(width: 380, alignment: .center)
                                 .foregroundColor(Color.white).padding(15).multilineTextAlignment(.center)
->>>>>>> b07eebeb0036d37c2f996bd9cb24926e82fd90e1
                             
                             Text("memellord@hustleruniversity.edu")
                                 .font(.custom("LexendDeca-Bold", size: 20))
                                 .frame(width: 380, height: 10, alignment: .center)
                                 .foregroundColor(.white)
-<<<<<<< HEAD
-                                .accentColor(.white).offset(x:20, y: -80).multilineTextAlignment(.center)
-=======
                                 .accentColor(.white).multilineTextAlignment(.center)
                                 .padding(15)
->>>>>>> b07eebeb0036d37c2f996bd9cb24926e82fd90e1
 
                                 
                         }

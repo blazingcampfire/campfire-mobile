c404294155585039b93a0c734b4dbbd306c5c909
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-06-27 20:30:35 -0500

    added prompts constant

---
+++ campfire-mobile/constants/prompts.swift
@@ -0,0 +1,20 @@
+//
+//  prompts.swift
+//  campfire-mobile
+//
+//  Created by Adarsh G on 6/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Prompts {
+    let prompts = [
+    "my school moment",
+    "my party face",
+    "top study spot",
+    "weekend outing",
+    "just another day at the office"
+    ]
+}

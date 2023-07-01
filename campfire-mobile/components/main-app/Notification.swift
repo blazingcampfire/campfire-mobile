0e7d2f396c2a803e5bf3326a4c0a3cd98122ed1c
toni-akintola <oakintol@nd.edu>
2023-06-30 23:38:31 -0400

    pull from dev

---
+++ campfire-mobile/components/main-app/Notification.swift
@@ -0,0 +1,52 @@
+//
+//  SwiftUIView.swift
+//  campfire-mobile
+//
+//  Created by Toni on 6/30/23.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    
+    var title: String = "Friend Request"
+    var content: String = "Adarsh Gadepalli has sent you a friend request!"
+    var timeStamp: String = "11:37am"
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            HStack {
+                Text(title)
+                    .padding(.leading, 10)
+                    .padding(.top, 5)
+                    .font(.custom("LexendDeca-Bold", size: 20))
+                Spacer()
+                Text(timeStamp)
+                    .padding(.trailing, 10)
+                    .padding(.top, 5)
+                    .font(.custom("LexendDeca-Regular", size: 15))
+                    
+            }
+            .foregroundColor(.white)
+            HStack {
+                Text(content)
+                    .padding(.leading, 10)
+                    .padding(.top, 5)
+                    .font(
+                    .custom("LexendDeca-Regular", size: 15))
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding(.bottom, 10)
+        }
+        .background(Theme.Peach)
+        .cornerRadius(8)
+        .padding()
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
---
+++ campfire-mobile/views/app-main-views/hub/NotificationsPage.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsPage.swift
+//  campfire-mobile
+//
+//  Created by Toni on 6/30/23.
+//
+
+import SwiftUI
+
+struct NotificationsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationsPage_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsPage()
+    }
+}

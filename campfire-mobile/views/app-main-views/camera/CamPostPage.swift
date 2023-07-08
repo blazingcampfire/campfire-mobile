400211f2ec2aa1bf0a518fad7f82c941f7c98a4c
davidadebogun <david.adebogun@yale.edu>
2023-07-07 22:03:12 -0400

    post button

--- campfire-mobile/views/app-main-views/camera/CamPostPage.swift
+++ campfire-mobile/views/app-main-views/camera/CamPostPage.swift
@@ -1,13 +1,11 @@
-//  TheFeed.swift
-//  campfire-mobile
-//
-//  Created by Adarsh G on 6/20/23.
-//
+
+
+
+
+
+
 import SwiftUI
 import AVKit
-
-
-
 struct CamPostPage: View {
     
     @State var currentVid = ""
@@ -55,7 +53,6 @@ struct CamPostPage: View {
 }
 //In this view a Tabview is iterating over the VidsPlayer View and setting up the vertical scroll ui component
 //VidsPlayer handles the specific actions of what each case should look like
-
 struct CamPostPage_Previews: PreviewProvider {
     static var previews: some View {
         CamPostPage()
@@ -166,9 +163,23 @@ struct CamPostPlayer: View {
                                         Text(userInfo.location)
                                             .font(.custom("LexendDeca-Regular", size: 15))
                                     }
+                                    .padding(.trailing, 240)
                              
                                 }
                                 .frame(alignment: .trailing)
+                                Button(action: {
+                            //post to feed
+                            }) {
+                                HStack(spacing: 7) {
+                                Text("post")
+                                .foregroundColor(.white)
+                                .font(.custom("LexendDeca-SemiBold", size: 22))
+                                Image(systemName: "arrowshape.right.fill")
+                                        .font(.system(size: 18))
+                                }
+                                .padding(15)
+                                .background(RoundedRectangle(cornerRadius: 40).fill(Theme.Peach))
+                                }
                             }
                 
                         }

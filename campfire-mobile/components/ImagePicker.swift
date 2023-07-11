6dd2b656b975a5c629581a157c40ae0ba41f0bbe
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-07-11 13:29:33 -0500

    image picker edits

--- campfire-mobile/components/ImagePicker.swift
+++ campfire-mobile/components/ImagePicker.swift
@@ -8,50 +8,68 @@
 import SwiftUI
 import Foundation
 import UIKit
+import PhotosUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     
     @Binding var selectedImage: Image?
     @Binding var isPickerShowing: Bool
     
-    func makeUIViewController(context: Context) -> some UIViewController {
-        let imagePicker = UIImagePickerController()
-        imagePicker.sourceType = .photoLibrary // use PHPicker
-        imagePicker.delegate = context.coordinator
-            
-        return imagePicker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
         
+        // this function initializes a PHPickerVC object that can be used for users to choose photo library images
+        
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        
+        return picker
     }
     
-    func makeCoordinator() -> Coordinator{
-        return Coordinator(self)
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
 
 
-class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class Coordinator: NSObject, PHPickerViewControllerDelegate, UINavigationControllerDelegate {
     
     var parent: ImagePicker
     
     init(_ picker: ImagePicker) {
         self.parent = picker
     }
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as?
-            UIImage {
-            let image = Image(uiImage: uiImage)
-            DispatchQueue.main.async {
-                self.parent.selectedImage = image
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let result = results.first else {
+            parent.isPickerShowing = false
+            return
+        }
+        
+        result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+            if let error = error {
+                print("Error loading image: \(error.localizedDescription)")
+            } else if let image = image as? UIImage {
+                DispatchQueue.main.async {
+                    self?.parent.selectedImage = Image(uiImage: image)
+                    self?.parent.isPickerShowing = false
+                }
+            } else {
+                print("Unable to load image.")
             }
         }
-        parent.isPickerShowing = false
     }
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    func pickerDidCancel(_ picker: PHPickerViewController) {
+        picker.dismiss(animated: true)
         parent.isPickerShowing = false
     }
     

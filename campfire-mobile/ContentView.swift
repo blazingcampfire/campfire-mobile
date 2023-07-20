4572e165147a8119206be7a9cda923646586080c
toni-akintola <oakintol@nd.edu>
2023-07-19 22:26:10 -0400

    contentView conditional change

--- campfire-mobile/ContentView.swift
+++ campfire-mobile/ContentView.swift
@@ -12,7 +12,7 @@ import FirebaseAuth
 struct ContentView: View {
     @StateObject var model = AuthModel()
     var body: some View {
-        if Auth.auth().currentUser?.uid != nil {
+        if Auth.auth().currentUser?.uid == nil {
             AccountSetUp()
                 .environmentObject(model)
                 .ignoresSafeArea(.keyboard, edges: .bottom)

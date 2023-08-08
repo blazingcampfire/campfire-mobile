6abb8dfe9a1457c873b0f18ed208c3f90a83b0af
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-08-07 19:42:06 -0500

    edit profile touch up

--- campfire-mobile/views/app-main-views/profile/EditProfile.swift
+++ campfire-mobile/views/app-main-views/profile/EditProfile.swift
@@ -59,36 +59,57 @@ struct EditProfile: View {
                                 HStack {
                                     Text(currentUser.profile.name)
                                         .font(.custom("LexendDeca-Bold", size: 15))
+                                    
                                     Spacer()
+                                    
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 30.0)
+                                            .foregroundColor(Theme.Peach)
+                                            .opacity(0.85)
+                                        
                                     NavigationLink(destination: EditFieldPage(maxCharacterLength: 20, unallowedCharacters: nameIllegalChar, field: "name", currentfield: currentUser.profile.name)
                                         .environmentObject(currentUser)) {
                                             Label("edit name", systemImage: "pencil")
                                                 .font(.custom("LexendDeca-Bold", size: 15))
-                                                .foregroundColor(Theme.Peach)
+                                                .foregroundColor(Color.white)
+                                        }
                                         }
                                 }
                                 HStack {
                                     Text(currentUser.profile.username)
                                         .font(.custom("LexendDeca-Bold", size: 15))
                                    Spacer()
-                                    NavigationLink(destination: EditFieldPage(maxCharacterLength: 20, unallowedCharacters: usernameIllegalChar, field: "username", currentfield: currentUser.profile.username)
-                                        .environmentObject(currentUser)) {
-                                            Label("edit username", systemImage: "pencil")
-                                                .font(.custom("LexendDeca-Bold", size: 15))
-                                                .foregroundColor(Theme.Peach)
-                                        }
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 30.0)
+                                            .foregroundColor(Theme.Peach)
+                                            .opacity(0.85)
+                                        
+                                        NavigationLink(destination: EditFieldPage(maxCharacterLength: 20, unallowedCharacters: usernameIllegalChar, field: "username", currentfield: currentUser.profile.username)
+                                            .environmentObject(currentUser)) {
+                                                Label("edit username", systemImage: "pencil")
+                                                    .font(.custom("LexendDeca-Bold", size: 15))
+                                                    .foregroundColor(Color.white)
+                                            }
+                                    }
                                 }
                                 HStack {
                                     Text(currentUser.profile.bio)
                                         .multilineTextAlignment(.center)
                                         .font(.custom("LexendDeca-Bold", size: 15))
+                                    
                                     Spacer()
-                                    NavigationLink(destination: EditFieldPage(maxCharacterLength: 50, field: "bio", currentfield: currentUser.profile.bio)
-                                        .environmentObject(currentUser)) {
-                                            Label("edit bio", systemImage: "pencil")
-                                                .font(.custom("LexendDeca-Bold", size: 15))
-                                                .foregroundColor(Theme.Peach)
-                                        }
+                                    
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 30.0)
+                                            .foregroundColor(Theme.Peach)
+                                            .opacity(0.85)
+                                        NavigationLink(destination: EditFieldPage(maxCharacterLength: 50, field: "bio", currentfield: currentUser.profile.bio)
+                                            .environmentObject(currentUser)) {
+                                                Label("edit bio", systemImage: "pencil")
+                                                    .font(.custom("LexendDeca-Bold", size: 15))
+                                                    .foregroundColor(Color.white)
+                                            }
+                                    }
                                 }
                             }
                             .padding(.leading, 20)

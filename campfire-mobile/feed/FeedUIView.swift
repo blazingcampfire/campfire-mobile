e7aaf81f05c5f4f60b38b472a16d8b478704cdd8
toni-akintola <oakintol@nd.edu>
2023-08-20 01:54:38 -0400

    merge for david

--- campfire-mobile/feed/FeedUIView.swift
+++ campfire-mobile/feed/FeedUIView.swift
@@ -180,10 +180,11 @@ struct FeedUIView: View {
             case .third:
                 EllipsesButtonView(equalIds: idsEqual)
                     .presentationDetents([.fraction(0.50)])
-                    .presentationDragIndicator(.visible)
                     .environmentObject(individualPost)
             case .fourth:
                 InitialMessage(school: currentUser.profile.school)
+                    .presentationDetents([.fraction(0.50)])
+                    .presentationDragIndicator(.visible)
             }
         }
 //        .alert(title: "Delete Post", message: "Are you sure you want to delete your post",

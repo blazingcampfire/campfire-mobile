1853a3de5193bd1046a98e1b92f57ddb235a10d1
toni-akintola <oakintol@nd.edu>
2023-08-21 11:20:54 -0400

    added delete document action

--- campfire-mobile/components/main-app/EllipsesButtonView.swift
+++ campfire-mobile/components/main-app/EllipsesButtonView.swift
@@ -89,11 +89,15 @@ struct EllipsesButtonView: View {
                             .padding(.top, 10)// Padding to move text away from edge
                         Spacer()
                         if selectedAction == "delete" {
-                            Image(systemName: "checkmark")
-                                .foregroundColor(Theme.Peach)
-                                .bold()
-                                .font(.system(size: 25))
-                                .padding(.trailing, 15)
+                            Button(action: {
+                                individualPostModel.deletePostDocument()
+                            }, label: {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(Theme.Peach)
+                                    .bold()
+                                    .font(.system(size: 25))
+                                    .padding(.trailing, 15)
+                            })
                         }
                     }
                 }

6d7d55ccf7892e7c3fbddf5c932d6048ea3d0d06
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-08-21 17:21:17 -0500

    added map

--- campfire-mobile/model/functions/globalFunctions.swift
+++ campfire-mobile/model/functions/globalFunctions.swift
@@ -70,11 +70,26 @@ func formatNumber(_ number: Int) -> String {
     }
 }
 
+
+
 func formatAddress(_ location: String) -> String {
-    if location == "7 Andrews View Ct, Windsor Mill" {
-        return "david house"
-    } else if location == "37 High St, New Haven" {
-        return "Sig Nu Yale"
+    let locationAddresses = [
+        "7 Andrews View Ct, Windsor Mill": "david house",
+        "37 High St, New Haven": "Sig Nu Yale",
+        "1605 Rice Blvd, Houston" : "McMurtry College",
+        "1601 Rice Blvd, Houston" : "Duncan College",
+        "9 Sunset Blvd, Houston" : "Brown College",
+        "23 Sunset Blvd, Houston" : "Jones College",
+        "99 Sunset Blvd, Houston" : "Martel College",
+        "6310 Main St, Houston" : "Lovett College",
+        "6320 Main St, Houston" : "Baker College",
+        "6360 Main St, Houston" : "Sid Richardson College",
+        "6340 Main St, Houston" : "Wiess College",
+        "6350 Main St, Houston" : "Hanszen College",
+    ]
+    
+    if let place = locationAddresses[location] {
+        return place
     } else {
         return location
     }

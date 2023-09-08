24bcf5630bcacacb3b67ff0331c8944a5aafaf4a
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2023-09-07 20:58:32 -0500

    push Noti functions

--- campfire-mobile/constants/unallowedCharacters.swift
+++ campfire-mobile/constants/unallowedCharacters.swift
@@ -6,40 +6,53 @@
 //
 
 import Foundation
+import FirebaseMessaging
 
 let usernameIllegalChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "`", "{", "|", "}", "~", " ", "'", ""]
 let nameIllegalChar = ["!", "\u{0022}", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "`", "{", "|", "}", "~", "_", "'", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "]
 
 
-//
-//func sendNotification(to fcmToken: String, title: String, body: String) {
-//    let urlString = "https://fcm.googleapis.com/fcm/send"
-//    let url = URL(string: urlString)!
-//    var request = URLRequest(url: url)
-//    request.httpMethod = "POST"
-//    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//    request.setValue("YOUR_SERVER_KEY", forHTTPHeaderField: "Authorization")
-//
-//    let message: [String: Any] = [
-//        "to": fcmToken,
-//        "notification": [
-//            "title": title,
-//            "body": body
-//        ]
-//    ]
-//
-//    do {
-//        let jsonData = try JSONSerialization.data(withJSONObject: message, options: [])
-//        request.httpBody = jsonData
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let error = error {
-//                print("Error sending notification: \(error)")
-//            } else {
-//                print("Notification sent successfully")
-//            }
-//        }.resume()
-//    } catch {
-//        print("Error creating JSON data: \(error)")
-//    }
-//}
+
+func sendNotification(to fcmToken: String, title: String, body: String) {
+    let urlString = "https://fcm.googleapis.com/fcm/send"
+    let url = URL(string: urlString)!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("YOUR_SERVER_KEY", forHTTPHeaderField: "Authorization")
+
+    let message: [String: Any] = [
+        "to": fcmToken,
+        "notification": [
+            "title": title,
+            "body": body
+        ]
+    ]
+
+    do {
+        let jsonData = try JSONSerialization.data(withJSONObject: message, options: [])
+        request.httpBody = jsonData
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error sending notification: \(error)")
+            } else {
+                print("Notification sent successfully")
+            }
+        }.resume()
+    } catch {
+        print("Error creating JSON data: \(error)")
+    }
+}
+
+func getToken {
+    Messaging.messaging().token { token, error in
+      if let error = error {
+        print("Error fetching FCM registration token: \(error)")
+      } else if let token = token {
+        print("FCM registration token: \(token)")
+        self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)"
+      }
+    }
+}
+
